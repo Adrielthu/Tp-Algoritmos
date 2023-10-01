f2845fa1ad918611673ad40cd1ed73c67de1bca6
Axel Escalante <axelescalante0@gmail.com>
2023-10-01 19:37:37 -0300

    implementacion de TemperaturasDB en entorno

---
+++ TP2_problema2/modulos/entorno.py
@@ -0,0 +1,12 @@
+from .AVL import AVL
+class TemperaturasDB:
+    def __init__(self):
+        self.avl = AVL()
+    
+    def guardar_temperatura(temperatura, fecha):
+        pass
+
+    def devolver_temperatura(fecha):
+        pass
+
+    
