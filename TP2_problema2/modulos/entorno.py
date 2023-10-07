1c073876c604df8b129a6f0d2c5cffb7110c2041
Adriel Starchevich <adrielthu@hotmail.com>
2023-10-07 15:14:44 -0300

    Agregando metodo _fecha_obj

--- TP2_problema2/modulos/entorno.py
+++ TP2_problema2/modulos/entorno.py
@@ -1,16 +1,24 @@
 from .AVL import AVL
+import datetime
 
 class TemperaturasDB:
     def __init__(self):
         self.avl = AVL()
 
+    def _fecha_obj(self, fecha):
+        fecha_obj = datetime.datetime.strptime(fecha, "%d/%m/%Y")
+        return fecha_obj
+
     def guardar_temperatura(self,temperatura, fecha):
-        self.avl.agregar(temperatura, fecha)
+        self.avl.agregar(self._fecha_obj(fecha), temperatura)
 
     def devolver_temperatura(self, fecha):
-        self.avl.obtener(fecha)
+        self.avl.obtener(self._fecha_obj(fecha))
 
     def max_temp_rango(self, fecha1, fecha2):
+        fecha1 = self._fecha_obj(fecha1)
+        fecha2 = self._fecha_obj(fecha2)
+
         max = self.avl.obtener(fecha1)
         for fecha in self.avl:
             if fecha >= fecha1 and fecha <=fecha2:
@@ -19,6 +27,9 @@ class TemperaturasDB:
         return max
 
     def min_temp_rango(self, fecha1, fecha2):
+        fecha1 = self._fecha_obj(fecha1)
+        fecha2 = self._fecha_obj(fecha2)
+
         min = self.avl.obtener(fecha1)
         for fecha in self.avl:
             if fecha >= fecha1 and fecha <=fecha2:
@@ -30,9 +41,12 @@ class TemperaturasDB:
         return self.min_temp_rango(fecha1, fecha2), self.max_temp_rango(fecha1,fecha2)
 
     def borrar_temperatura(self, fecha):
-        self.avl.eliminar(fecha)
+        self.avl.eliminar(self._fecha_obj(fecha))
 
     def devolver_temperaturas(self, fecha1, fecha2):
+        fecha1 = self._fecha_obj(fecha1)
+        fecha2 = self._fecha_obj(fecha2)
+
         for fecha in self.avl:
             if fecha >= fecha1 and fecha <=fecha2:
                 print(f'{fecha.clave}: temperatura {fecha.cargaUtil}°C')
