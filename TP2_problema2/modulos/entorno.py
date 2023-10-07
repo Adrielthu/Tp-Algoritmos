56833f6714b6e2737d0311ba27da7bc23b5ef113
Adriel Starchevich <adrielthu@hotmail.com>
2023-10-07 14:13:42 -0300

    Completando metodos del entorno

--- TP2_problema2/modulos/entorno.py
+++ TP2_problema2/modulos/entorno.py
@@ -1,25 +1,41 @@
 from .AVL import AVL
+
 class TemperaturasDB:
     def __init__(self):
         self.avl = AVL()
-    
+
     def guardar_temperatura(self,temperatura, fecha):
         self.avl.agregar(temperatura, fecha)
 
     def devolver_temperatura(self, fecha):
         self.avl.obtener(fecha)
-    
+
+    def max_temp_rango(self, fecha1, fecha2):
+        max = self.avl.obtener(fecha1)
+        for fecha in self.avl:
+            if fecha >= fecha1 and fecha <=fecha2:
+                if self.avl.obtener(fecha) > max:
+                    max = self.avl.obtener(fecha)
+        return max
+
     def min_temp_rango(self, fecha1, fecha2):
-        pass
+        min = self.avl.obtener(fecha1)
+        for fecha in self.avl:
+            if fecha >= fecha1 and fecha <=fecha2:
+                if self.avl.obtener(fecha) < min:
+                    min = self.avl.obtener(fecha)
+        return min
 
     def temp_extremos_rango(self, fecha1, fecha2):
-        pass
+        return self.min_temp_rango(fecha1, fecha2), self.max_temp_rango(fecha1,fecha2)
 
     def borrar_temperatura(self, fecha):
-        pass
+        self.avl.eliminar(fecha)
 
     def devolver_temperaturas(self, fecha1, fecha2):
-        pass
+        for fecha in self.avl:
+            if fecha >= fecha1 and fecha <=fecha2:
+                print(f'{fecha.clave}: temperatura {fecha.cargaUtil}°C')
 
     def cantidad_muestras(self):
         return self.avl.tamano
