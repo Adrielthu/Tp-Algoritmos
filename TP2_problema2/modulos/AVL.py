e7251aa4033ae73bbb6ff95965764adf48b17395
Adriel Starchevich <adrielthu@hotmail.com>
2023-10-16 16:34:28 -0300

    Separación de Clases

--- TP2_problema2/modulos/AVL.py
+++ TP2_problema2/modulos/AVL.py
@@ -1,88 +1,5 @@
-class NodoArbol:
-    def __init__(self, clave, valor, padre=None, izquierdo=None, derecho=None):
-        self.clave = clave
-        self.cargaUtil = valor
-        self.hijoDerecho = derecho
-        self.hijoIzquierdo = izquierdo
-        self.padre = padre
-        self.factorEquilibrio = 0
-
-    def tieneHijoIzquierdo(self):
-        return self.hijoIzquierdo
-
-    def tieneHijoDerecho(self):
-        return self.hijoDerecho
-
-    def esHijoIzquierdo(self):
-        return self.padre and self.padre.hijoIzquierdo == self
-
-    def esHijoDerecho(self):
-        return self.padre and self.padre.hijoDerecho == self
-
-    def esRaiz(self):
-        return not self.padre
-
-    def esHoja(self):
-        return not (self.hijoDerecho or self.hijoIzquierdo)
-
-    def tieneAlgunHijo(self):
-        return self.hijoDerecho or self.hijoIzquierdo
-
-    def tieneAmbosHijos(self):
-        return self.hijoDerecho and self.hijoIzquierdo
-
-    def reemplazarDatoDeNodo(self, clave, valor, hizq, hder):
-        self.clave = clave
-        self.cargaUtil = valor
-        self.hijoIzquierdo = hizq
-        self.hijoDerecho = hder
-        if self.tieneHijoIzquierdo():
-            self.hijoIzquierdo.padre = self
-        if self.tieneHijoDerecho():
-            self.hijoDerecho.padre = self
-
-    def encontrarMin(self):
-        actual = self
-        while actual.tieneHijoIzquierdo():
-            actual = actual.hijoIzquierdo
-        return actual
-
-    def encontrarSucesor(self):
-        suc = None
-        if self.tieneHijoDerecho():
-            suc = self.hijoDerecho.encontrarMin()
-        else:
-            if self.padre:
-                if self.esHijoIzquierdo():
-                    suc = self.padre
-                else:
-                    self.padre.hijoDerecho = None
-                    suc = self.padre.encontrarSucesor()
-                    self.padre.hijoDerecho = self
-        return suc
-
-    def empalmar(self):
-        if self.esHoja():
-            if self.esHijoIzquierdo():
-                self.padre.hijoIzquierdo = None
-            else:
-                self.padre.hijoDerecho = None
-        elif self.tieneAlgunHijo():
-            if self.tieneHijoIzquierdo():
-                if self.esHijoIzquierdo():
-                    self.padre.hijoIzquierdo = self.hijoIzquierdo
-                else:
-                    self.padre.hijoDerecho = self.hijoIzquierdo
-                self.hijoIzquierdo.padre = self.padre
-            else:
-                if self.esHijoIzquierdo():
-                    self.padre.hijoIzquierdo = self.hijoDerecho
-                else:
-                    self.padre.hijoDerecho = self.hijoDerecho
-                self.hijoDerecho.padre = self.padre
-
 # --------------------------------------------------------------------------------------------------------------
-
+from nodoArbol import NodoArbol
 # --------------------------------------------------------------------------------------------------------------
 
 class AVL:
@@ -123,6 +40,14 @@ class AVL:
 
             if nodo.padre.factorEquilibrio != 0:
                 self.actualizarEquilibrio(nodo.padre)
+
+    def _actualizarEquilibrioEliminar(self, nodoAEliminar):
+        if nodoAEliminar.esHijoIzquierdo():
+            nodoAEliminar.padre.factorEquilibrio -= 1
+        elif nodoAEliminar.esHijoDerecho():
+            nodoAEliminar.padre.factorEquilibrio += 1
+        if nodoAEliminar.padre != None:
+            self._actualizarEquilibrioEliminar(nodoAEliminar.padre)
 
     def rotarIzquierda(self, rotRaiz):
         # La nueva raiz va a ser el hijo derecho de la raiz actual
@@ -228,7 +153,13 @@ class AVL:
         if self.tamano > 1:
             nodoAEliminar = self._obtener(clave, self.raiz)
             if nodoAEliminar:
+
+                padreNodoAEliminar = nodoAEliminar.padre
+                self._actualizarEquilibrioEliminar(nodoAEliminar)
                 self.remover(nodoAEliminar)
+                if padreNodoAEliminar.factorEquilibrio > 1 or padreNodoAEliminar.factorEquilibrio < -1:
+                    self.reequilibrar(padreNodoAEliminar)
+
                 self.tamano = self.tamano - 1
             else:
                 raise KeyError("Error, la clave no está en el árbol")
@@ -287,4 +218,3 @@ class AVL:
 # --------------------------------------------------------------------------------------------------------------
 
 # --------------------------------------------------------------------------------------------------------------
-
---
+++ TP2_problema2/modulos/nodoArbol.py
@@ -0,0 +1,82 @@
+class NodoArbol:
+    def __init__(self, clave, valor, padre=None, izquierdo=None, derecho=None):
+        self.clave = clave
+        self.cargaUtil = valor
+        self.hijoDerecho = derecho
+        self.hijoIzquierdo = izquierdo
+        self.padre = padre
+        self.factorEquilibrio = 0
+
+    def tieneHijoIzquierdo(self):
+        return self.hijoIzquierdo
+
+    def tieneHijoDerecho(self):
+        return self.hijoDerecho
+
+    def esHijoIzquierdo(self):
+        return self.padre and self.padre.hijoIzquierdo == self
+
+    def esHijoDerecho(self):
+        return self.padre and self.padre.hijoDerecho == self
+
+    def esRaiz(self):
+        return not self.padre
+
+    def esHoja(self):
+        return not (self.hijoDerecho or self.hijoIzquierdo)
+
+    def tieneAlgunHijo(self):
+        return self.hijoDerecho or self.hijoIzquierdo
+
+    def tieneAmbosHijos(self):
+        return self.hijoDerecho and self.hijoIzquierdo
+
+    def reemplazarDatoDeNodo(self, clave, valor, hizq, hder):
+        self.clave = clave
+        self.cargaUtil = valor
+        self.hijoIzquierdo = hizq
+        self.hijoDerecho = hder
+        if self.tieneHijoIzquierdo():
+            self.hijoIzquierdo.padre = self
+        if self.tieneHijoDerecho():
+            self.hijoDerecho.padre = self
+
+    def encontrarMin(self):
+        actual = self
+        while actual.tieneHijoIzquierdo():
+            actual = actual.hijoIzquierdo
+        return actual
+
+    def encontrarSucesor(self):
+        suc = None
+        if self.tieneHijoDerecho():
+            suc = self.hijoDerecho.encontrarMin()
+        else:
+            if self.padre:
+                if self.esHijoIzquierdo():
+                    suc = self.padre
+                else:
+                    self.padre.hijoDerecho = None
+                    suc = self.padre.encontrarSucesor()
+                    self.padre.hijoDerecho = self
+        return suc
+
+    def empalmar(self):
+        if self.esHoja():
+            if self.esHijoIzquierdo():
+                self.padre.hijoIzquierdo = None
+            else:
+                self.padre.hijoDerecho = None
+        elif self.tieneAlgunHijo():
+            if self.tieneHijoIzquierdo():
+                if self.esHijoIzquierdo():
+                    self.padre.hijoIzquierdo = self.hijoIzquierdo
+                else:
+                    self.padre.hijoDerecho = self.hijoIzquierdo
+                self.hijoIzquierdo.padre = self.padre
+            else:
+                if self.esHijoIzquierdo():
+                    self.padre.hijoIzquierdo = self.hijoDerecho
+                else:
+                    self.padre.hijoDerecho = self.hijoDerecho
+                self.hijoDerecho.padre = self.padre
