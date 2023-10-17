7ebdf70b4f3a73662dc9fbffa00a025f76239ef5
Adriel Starchevich <adrielthu@hotmail.com>
2023-10-17 12:03:02 -0300

    Agregando el método repr

--- TP2_problema3/modulos/vertice.py
+++ TP2_problema3/modulos/vertice.py
@@ -1,4 +1,4 @@
- 
+
 class Vertice:
     def __init__(self,clave):
         self.id = clave
@@ -8,13 +8,14 @@ class Vertice:
         self.visitado = False
         self.color = 'Blanco'
 
-        
-
     def agregarVecino(self,vecino,ponderacion=0):
         self.conectadoA[vecino] = ponderacion
 
     def __str__(self):
-        return str(self.id) + ' conectadoA: ' + str([x.id for x in self.conectadoA])
+        return str(self.id)
+
+    def __repr__(self):
+        return self.__str__()
 
     def obtenerConexiones(self):
         return self.conectadoA.keys()
@@ -24,19 +25,19 @@ class Vertice:
 
     def obtenerPonderacion(self,vecino):
         return self.conectadoA[vecino]
-    
+
     def asignarPredecesor(self,predecesor):
         self.predecesor = predecesor
 
     def obtenerPredecesor(self):
         return self.predecesor
-    
+
     def asignarDistancia(self, distancia):
         self.distancia = distancia
-    
+
     def obtenerDistancia(self):
         return self.distancia
-    
+
     def recorrer(y):
         x = y
         while (x.obtenerPredecesor()):
