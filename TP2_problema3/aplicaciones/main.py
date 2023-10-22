a5611901414b6e16b22bbbd9bb5a62abed42b1de
Adriel Starchevich <adrielthu@hotmail.com>
2023-10-22 09:26:31 -0300

    Actualizando cambios del entorno

--- TP2_problema3/aplicaciones/main.py
+++ TP2_problema3/aplicaciones/main.py
@@ -1,18 +1,18 @@
-from TP2_problema3.modulos.entorno import EntornoRutas
+from TP2_problema3.modulos.entorno import ServicioDeTransporte
 
-ejemplo = EntornoRutas()
+ejemplo = ServicioDeTransporte()
 
-# Recibe como parámetro un archivo
-ejemplo.construir_grafo("rutas.txt")
+# Recibe como parámetro un archivo y crea el grafo
+ejemplo.construir_rutas("rutas.txt")
 
-ejemplo.numeroDeVertices()
+# Retorna el numero de vertices
+ejemplo.numeroDeCiudades()
 
-ejemplo.mostrar_caminos()
+#ejemplo.mostrar_caminos()
 
 #Recibe como parámetro una ciudad de inicio y una de destino
-ejemplo.precio_min("CiudadBs.As.", "VillaMercedes")
+ejemplo.precio_min("CiudadBs.As.", "LaRioja")
 
+#ejemplo.graficar(usar_grafo_precio=True)
 
-# ejemplo.graficar(usar_grafo_precio=True)
-
-# ejemplo.graficar(usar_grafo_precio=False)
+#ejemplo.graficar(usar_grafo_precio=False)
