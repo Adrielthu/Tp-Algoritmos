e5f2dca59301d6a1690242ec1bd7da2be5244c26
Adriel Starchevich <adrielthu@hotmail.com>
2023-10-22 21:49:01 -0300

    Adaptando el codigo al nuevo grafo

--- TP2_problema3/modulos/entorno.py
+++ TP2_problema3/modulos/entorno.py
@@ -2,59 +2,51 @@ from TP2_problema3.modulos.grafo import Grafo
 import matplotlib.pyplot as plt
 import networkx as nx
 
-class EntornoRutas():
-    def __init__(self):
-        self.grafo_peso = Grafo()
-        self.grafo_precio = Grafo()
 
-    def construir_grafo(self,archivo):
-        with open(archivo,'r') as rutas:
+class ServicioDeTransporte:
+    def __init__(self):
+        self.grafo = Grafo()
+
+    def construir_grafo(self, archivo):
+        with open(archivo, "r") as rutas:
             lectura = rutas.readlines()
 
             tabla = []
             for ruta in lectura:
-                dato = ruta.strip().split(',')
-                if dato[0] not in self.grafo_peso:
-                    self.grafo_peso.agregarVertice(dato[0])
-                    # self.grafo_precio.agregarVertice(dato[0])
+                dato = ruta.strip().split(",")
+                if dato[0] not in self.grafo:
+                    self.grafo.agregarVertice(dato[0])
                 tabla.append(dato)
         for fila in tabla:
             salida = fila[0]
             destino = fila[1]
             peso = int(fila[2])
             precio = int(fila[3])
-            self.grafo_peso.agregarArista(salida,destino,peso,precio)
-            # self.grafo_precio.agregarArista(salida,destino,precio)
+            self.grafo.agregarArista(salida, destino, peso, precio)
 
     def numeroDeVertices(self):
-        print(self.grafo_peso.numVertices)
+        print(self.grafo.numVertices)
 
     def mostrar_caminos(self):
-        print(self.grafo_peso)
+        print(self.grafo)
 
-    def precio_min(self,inicio,destino):
-        prueba = self.grafo_peso.camino(self.grafo_peso,inicio,destino)
-        return print(prueba)
+    def precio_min(self, inicio, destino):
+        prueba = self.grafo.camino(self.grafo, inicio, destino)
 
-
-#--------------------------------------------------------------------------------------------------------
-#                                           REVISAR
-#--------------------------------------------------------------------------------------------------------
-
-    def graficar(self, usar_grafo_precio=True):
-        if usar_grafo_precio:
-            grafo = self.grafo_precio
+        if isinstance(prueba, RuntimeError):
+            print(f"Error: {prueba}")
         else:
-            grafo = self.grafo_peso
-        self._grafica(grafo)
+            print(
+                f"El camino de {inicio} a {destino} es\n{prueba[0]}\nEste es el peso\n{prueba[1]}\nY el precio\n{prueba[2]}"
+            )
 
-    def _grafica(self,grafo):
+    def graficar(self):
         G = nx.DiGraph()
-        for vertice in grafo.obtenerVertices():
+        for vertice in self.grafo.obtenerVertices():
             G.add_node(vertice)
 
         # Agrega aristas con dos atributos de ponderación
-        for vertice in grafo:
+        for vertice in self.grafo:
             for vecino in vertice.obtenerConexiones():
                 salida = vertice.obtenerId()
                 destino = vecino.obtenerId()
@@ -65,12 +57,17 @@ class EntornoRutas():
 
         # Dibuja el gráfico
         pos = nx.spring_layout(G)
-        labels = {edge: (G.edges[edge]['peso'], G.edges[edge]['precio']) for edge in G.edges}
-        nx.draw(G, pos, with_labels=True, node_color='skyblue', font_weight='bold', node_size=100)
+        labels = {
+            edge: (G.edges[edge]["peso"], G.edges[edge]["precio"]) for edge in G.edges
+        }
+        nx.draw(
+            G,
+            pos,
+            with_labels=True,
+            node_color="skyblue",
+            font_weight="bold",
+            node_size=100,
+        )
         nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
 
         plt.show()
-
-#--------------------------------------------------------------------------------------------------------
-
-#--------------------------------------------------------------------------------------------------------
